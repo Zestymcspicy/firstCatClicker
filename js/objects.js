4ed2b012e25904d1aa3792df4c219bbc0b22c006
Harold Beckerman <halbeckerman@gmail.com>
2018-07-20 13:04:23 -0500

    working with more than two cats, going to attempt to build the DOM elements in the object constructor function

--- js/objects.js
+++ js/objects.js
@@ -1,25 +1,36 @@
+let imageArray = [];
+let builtArray= [];
+
 class AnimalImage {
   constructor(name, imageLocation) {
     this.name = name;
     this.imageLocation = imageLocation;
+    this.clickCount = 0;
+    imageArray.push(this);
   };
 };
 
 const mainContainer = document.getElementById("mainContent");
 const larry = new AnimalImage("Larry Shonsleberry", "images/IMG_1278.png");
 const chewie = new AnimalImage("Chewbacca", "images/chewie.png");
-
-let imageArray = [larry, chewie];
+const twoKitties = new AnimalImage("Cuddling Kittens", "images/twoKitties.png");
 
 function buildImage (animal) {
+  let countSpan = document.createElement("span");
   let newDiv = document.createElement("div");
   let newText = document.createTextNode(`${animal.name}`);
   let newImage = document.createElement("img");
   newImage.src=`${animal.imageLocation}`;
   newDiv.appendChild(newText);
+  newDiv.appendChild(countSpan);
   newDiv.appendChild(newImage);
   mainContainer.appendChild(newDiv);
-  newDiv.classList.add('animal-card')
+  newDiv.classList.add('animal-card');
+  builtArray.push(newDiv);
+  newImage.addEventListener('click', function (){
+    animal.clickCount++;
+    countSpan.innerHTML = (`${animal.clickCount}`);
+  });
 };
 
 imageArray.forEach(function (element){
