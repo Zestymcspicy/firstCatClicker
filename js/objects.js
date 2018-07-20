23386bfe51885862d6f16f3521f5eb6d157eb979
Harold Beckerman <halbeckerman@yahoo.com>
2018-07-20 01:19:30 -0500

    continuing to style

--- js/objects.js
+++ js/objects.js
@@ -5,21 +5,21 @@ class AnimalImage {
   };
 };
 
-const littleContainer = document.getElementById("mainContent");
+const mainContainer = document.getElementById("mainContent");
 const larry = new AnimalImage("Larry Shonsleberry", "images/IMG_1278.png");
 const chewie = new AnimalImage("Chewbacca", "images/chewie.png");
 
 let imageArray = [larry, chewie];
 
 function buildImage (animal) {
-  let thisDiv = document.createElement("div");
+  let newDiv = document.createElement("div");
   let newText = document.createTextNode(`${animal.name}`);
   let newImage = document.createElement("img");
   newImage.src=`${animal.imageLocation}`;
-  thisDiv.appendChild(newText);
-  thisDiv.appendChild(newImage);
-  littleContainer.appendChild(thisDiv);
-  thisDiv.classList.add('animal-card')
+  newDiv.appendChild(newText);
+  newDiv.appendChild(newImage);
+  mainContainer.appendChild(newDiv);
+  newDiv.classList.add('animal-card')
 };
 
 imageArray.forEach(function (element){
